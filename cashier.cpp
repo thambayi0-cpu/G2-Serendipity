e4713cb31c219da4bb2ef68fb3e0a0d8d48704eb
Thokozile Hambayi <thambayi0@saddleback.edu>
2025-09-20 17:33:23 -0700

    added proper receipt formatting

--- cashier.cpp
+++ cashier.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 void cashier()
 {
-	const int TOTAL_WIDTH = 80;
+	const int TOTAL_WIDTH = 79;
 	const int QTY_WIDTH = 3;
 	const int ISBN_WIDTH = 13;
 	const int TITLE_WIDTH = 37;
 	const int PRICE_WIDTH = 8;
-	const int PRICE_TOTAL_WIDTH = 8;
+	const int PRICETOTAL_WIDTH = 8;
 
 	string date;
 	string title;
@@ -43,27 +43,43 @@ void cashier()
 
 	cout << "\033[H\033[2J";
 
-	cout << setfill('-') << setw(TOTAL_WIDTH) << "-" << endl << setfill(' ');
-	cout << left << setw(TOTAL_WIDTH-1) << "-" << "-" << endl;
-	cout << setw(TOTAL_WIDTH-1) << "-Serendipity Booksellers" << "-" << endl;
-	cout << setw(TOTAL_WIDTH-1) << "-" << "-" << endl;
-	cout << "-Date: " << date << right << setw(TOTAL_WIDTH - 7 - date.size()) << "-" << endl;
-	cout << setfill('-') << setw(TOTAL_WIDTH) << "-" << endl << setfill(' ');
+	cout << right << "+" << setfill('-') << setw(TOTAL_WIDTH) << "+" << endl << setfill(' ') << left;
 
-	cout << fixed << endl;
-	cout << "Date: " << date << endl;
-	cout << "Quantity of Book: " << setprecision(0) << qty << endl;
-	cout << "ISBN: " << isbn << endl;
-	cout << "Title: " << title << endl;
-	cout << "Price: $" << setprecision(2) << price << endl;
+	cout << setw(TOTAL_WIDTH) << "|" << "|" << endl;
 
-	cout << '\n';
-	cout << "Subtotal: $" << subTotal << endl;
-	cout << "Tax: $" << tax << endl;
-	cout << "Total: $" << total << endl;
+	cout << setw(TOTAL_WIDTH) << "|Serendipity Booksellers" << "|" << endl;
 
+	cout << setw(TOTAL_WIDTH) << "|" << "|" << endl;
+
+	cout << "|Date: " << date << right << setw(TOTAL_WIDTH - 6 - date.size()) << "|" << endl;
+
+	cout << left << setw(TOTAL_WIDTH) << "|" << "|" << endl;
+
+	cout << right << "+" << setfill('-') << setw(TOTAL_WIDTH) << "+" << endl << setfill(' ') << left;
+
+	cout << "|Qty  " << setw(ISBN_WIDTH) << "ISBN" << " " << setw(TITLE_WIDTH) << "Title" << " " << setw(PRICE_WIDTH) << "Price" << "    " << setw(PRICETOTAL_WIDTH) << "Total" << " |" << endl;
+
+	cout << right << "+" << setfill('-') << setw(TOTAL_WIDTH) << "+" << endl << setfill(' ') << left;
+
+	cout << fixed;
+
+	cout << "|" << right << setprecision(0) << setw(QTY_WIDTH) << qty << "  " << left << setw(ISBN_WIDTH) << isbn << " " << setw(TITLE_WIDTH) << title << " $" << right << setw(PRICE_WIDTH-1) << setprecision(2) << price << "    $" << setw(PRICETOTAL_WIDTH-1) << subTotal << " |" << left << endl;
+
+	cout << setw(TOTAL_WIDTH) << "|" << "|" << endl;
+
+	cout << setw(TOTAL_WIDTH) << "|" << "|" << endl;
+
+	cout << left << setw(QTY_WIDTH+ISBN_WIDTH+TITLE_WIDTH+5) << "|" << setw(PRICE_WIDTH) << "Subtotal" << "    $" << right << setw(PRICETOTAL_WIDTH-1) << subTotal << " |" << left << endl;
+
+	cout << left << setw(QTY_WIDTH+ISBN_WIDTH+TITLE_WIDTH+5) << "|" << setw(PRICE_WIDTH) << "Tax" << "    $" << right << setw(PRICETOTAL_WIDTH-1) << tax << " |" << left << endl;
+
+	cout << left << setw(QTY_WIDTH+ISBN_WIDTH+TITLE_WIDTH+5) << "|" << setw(PRICE_WIDTH) << "Total" << "    $" << right << setw(PRICETOTAL_WIDTH-1) << total << " |" << left << endl;
+
+	cout << setw(TOTAL_WIDTH) << "|" << "|" << endl;
+
+	cout << setw(TOTAL_WIDTH) << "| Thank you for shopping at Serendipity!" << "|" << endl;
+
+	cout << right << "+" << setfill('-') << setw(TOTAL_WIDTH) << "+" << endl << setfill(' ') << left;
 
 	return;
 }
-
-#include "cashier.h"
