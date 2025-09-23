5a7a8410436083e29823091e3c33eff6ca75b689
Ahmer Yasin <ayasin0@ivc.edu>
2025-09-23 03:31:23 -0700

    Added repCost.cpp stub

---
+++ repCost.cpp
@@ -0,0 +1,20 @@
+/***************************************************************************
+* PROGRAMMED BY : Ahmer Yasin & Thokozile Hambayi
+* ID            : A00349012   & 127485
+* CLASS         : CS1B
+* SECTION       : TTh 1:30pm
+* MODULE        : 2
+* GROUP PROJECT : 2
+* PROGRAM NAME  : Menus (Navigation & Stubs)
+* PURPOSE       : Bookstore POS
+* DUE DATE      : 09/23/2025
+****************************************************************************/
+
+#include "format.h"
+#include "repCost.h"
+
+int repCost (bool &keepReportsMenuActive, bool &keepRepCosttMenuActive)
+{
+	return 0;
+}
+
